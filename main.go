5b118e03f4c63d8fff879e9502e3611b2d075fc4
Hubbard Harvey <Hubbard.harvey@gmail.com>
2022-02-04 21:04:45 -0500

    Finished the levels with input from the boys.

--- main.go
+++ main.go
@@ -2,22 +2,78 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math/rand"
+	"strconv"
+	"time"
+	"os"
 )
 
-var path string
+var name string
+
+
 
 func main() {
-	fmt.Println("Welcome to Xpath!  Choose your path: Easy, Medium or Hard")
-	fmt.Print("Type your option: ")
-	fmt.Scanln(&path)
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("Welcome to Xpath!")
+	fmt.Println("What is your Name?")
+	fmt.Scan(&name)
 
-	path = strings.ToLower(path)
-
-	if path == "easy" || path == "medium" || path == "hard" {
-		fmt.Printf("Great, you chose %v\n", path)
+	// Go to the desert
+	fmt.Printf("Welcome to the desert %v. \n", name)
+	fmt.Printf(" 𝞇 𝞇 𝞇𝞇𝞇     𝞇   𝞇𝞇 𝞇 \n")
+	fmt.Printf(" 𝞇           𝞇  𝞇𝞇          𝞇\n")
+	fmt.Printf(" 𝞇 𝞇 𝞇          𝞇                 𝞇      𝞇   \n")
+	fmt.Println("There are riddles you need to solve.")
+	var answer string
+	var ans int
+	var x int = rand.Intn(10)
+	var y int = rand.Intn(8)
+	fmt.Printf("What is %v + %x?", x, y)
+	fmt.Scan(&answer)
+	ans, _ = strconv.Atoi(answer)
+	if ans == (x+y) {
+		fmt.Println("YOU DID IT!!! You can move on to the next round")
 	} else {
-		fmt.Println("So you have chosen death...")
+		fmt.Println("Slowly, you die of thrist and your world slowly dims dark as you thrist to death.")
+		os.Exit(1)
 	}
 
+	// the Forest Level
+	fmt.Printf("Welcome to the forest %v.\n", name)
+	fmt.Printf("🌳  🌳🌳🌳\n")
+	fmt.Printf("🌳🌳     🌲🌴 🌳\n")
+	fmt.Printf("🌳🌳🌳🌳🌴🌴\n")
+	fmt.Printf("🌳🌳   🌴 🌳🌳🌳\n")
+	rand.Seed(time.Now().UnixNano())
+	x = rand.Intn(20)
+	y = rand.Intn(10)
+	fmt.Printf("What is %v + %x? \n", x, y)
+	fmt.Scan(&answer)
+	ans, _ = strconv.Atoi(answer)
+	if ans == (x+y) {
+		fmt.Println("YOU DID IT!!! You can move out of the forest and onto the next round")
+	} else {
+		fmt.Println("Slowly, the trees close in and you become disoriented before succombing to your hunger.")
+		os.Exit(1)
+	}
+
+	// the Swamp
+	fmt.Printf("Welcome to the swamp %v. Where only GATORS get out alive\n", name)
+	fmt.Printf("🐊🐊    \n")
+	fmt.Printf("🐸\n")
+	fmt.Printf("🌩️  ⛈️    ⛈️    \n")
+	fmt.Printf("🐊🐊    \n")
+	rand.Seed(time.Now().UnixNano())
+	x = rand.Intn(30)
+	y = rand.Intn(10)
+	fmt.Printf("What is %v + %x? \n", x, y)
+	fmt.Scan(&answer)
+	ans, _ = strconv.Atoi(answer)
+	if ans == (x+y) {
+		fmt.Println("YOU DID IT!!! YOU BEAT THE GAME!!!!! ")
+		fmt.Println("You did great, you should play again.")
+	} else {
+		fmt.Println("Your last misstep spells certain doom as you flounder in the water and gators creep in to devour you.")
+		os.Exit(1)
+	}
 }
